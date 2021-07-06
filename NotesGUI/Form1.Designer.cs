c11713225a764d882706b152f35cba97fe127857
Владислав Асекретов <vladasekretov@gmail.com>
2021-07-06 18:00:14 +0300

    add GUI files

--- NotesGUI/Form1.Designer.cs
+++ NotesGUI/Form1.Designer.cs
@@ -1,5 +1,4 @@
-﻿
-namespace NotesGUI
+﻿namespace NotesGUI
 {
     partial class Form1
     {
@@ -29,13 +28,24 @@ namespace NotesGUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 152);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOX";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(450, 450);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -43,6 +53,7 @@ namespace NotesGUI
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
-
